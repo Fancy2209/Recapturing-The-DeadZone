614e91c2603b25523dbd3738ed3cf403ca2536a7
glennhenry <glennhenrybdg05@gmail.com>
2025-07-12 00:53:28 +0700

    Move relevant HTTP configuration to HTTP.kt, setup empty API responses

--- src/main/kotlin/module/HTTP.kt
+++ src/main/kotlin/module/HTTP.kt
@@ -3,15 +3,18 @@ package dev.deadzone.module
 import io.ktor.http.*
 import io.ktor.server.application.*
 import io.ktor.server.plugins.cors.routing.*
+import io.ktor.server.plugins.statuspages.StatusPages
+import io.ktor.server.response.respondText
 
 fun Application.configureHTTP() {
     install(CORS) {
-        allowMethod(HttpMethod.Options)
-        allowMethod(HttpMethod.Put)
-        allowMethod(HttpMethod.Delete)
-        allowMethod(HttpMethod.Patch)
-        allowHeader(HttpHeaders.Authorization)
-        allowHeader("MyCustomHeader")
-        anyHost() // @TODO: Don't do this in production if possible. Try to limit it.
+        allowHost("localhost:8080", schemes = listOf("http"))
+        allowHeader(HttpHeaders.ContentType)
+        allowMethod(HttpMethod.Get)
+    }
+    install(StatusPages) {
+        exception<Throwable> { call, cause ->
+            call.respondText(text = "500: $cause" , status = HttpStatusCode.InternalServerError)
+        }
     }
 }
--- src/main/kotlin/module/Routing.kt
+++ src/main/kotlin/module/Routing.kt
@@ -1,20 +1,20 @@
 package dev.deadzone.module
 
-import io.ktor.http.*
 import io.ktor.server.application.*
 import io.ktor.server.http.content.*
-import io.ktor.server.plugins.statuspages.*
-import io.ktor.server.response.*
+import io.ktor.server.response.respondText
 import io.ktor.server.routing.*
 
 fun Application.configureRouting() {
-    install(StatusPages) {
-        exception<Throwable> { call, cause ->
-            call.respondText(text = "500: $cause" , status = HttpStatusCode.InternalServerError)
-        }
-    }
-
     routing {
         staticResources("/", "static")
+
+        post("/api/13") {
+            call.respondText("asdfsd")
+        }
+
+        post("/api") {
+            call.respondText("API 0")
+        }
     }
 }
