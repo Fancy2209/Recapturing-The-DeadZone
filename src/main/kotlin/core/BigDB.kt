3f6136b2a6f6674807dd1f309dbb631a1b04926b
glennhenry <glennhenrybdg05@gmail.com>
2025-07-15 15:44:11 +0700

    Add getDB method for easy access temporarily

--- src/main/kotlin/core/BigDB.kt
+++ src/main/kotlin/core/BigDB.kt
@@ -4,8 +4,17 @@ import com.mongodb.kotlin.client.coroutine.MongoCollection
 import com.mongodb.kotlin.client.coroutine.MongoDatabase
 import org.bson.Document
 
+// Start MongoDB community edition (local)
+// "C:\Program Files\MongoDB\Server\8.0\bin\mongod.exe" --dbpath="c:\data\db"
+// use mongod if installed in PATH
+// Must be cmd in Windows not powershell
 class BigDB(private val db: MongoDatabase) {
     fun get(collection: String): MongoCollection<Document> {
         return db.getCollection(collection)
+    }
+
+    // temporary for easy db access
+    fun getDB(): MongoDatabase {
+        return db
     }
 }
