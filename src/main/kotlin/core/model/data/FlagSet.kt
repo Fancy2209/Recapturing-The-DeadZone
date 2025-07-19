aeb8fc65a1c9509ae905c2c42f7522fc36c944a6
glennhenry <glennhenrybdg05@gmail.com>
2025-07-19 21:43:17 +0700

    Use real data type for FlagSet. Minimize data guessing in login state.

--- src/main/kotlin/core/model/data/FlagSet.kt
+++ src/main/kotlin/core/model/data/FlagSet.kt
@@ -5,4 +5,18 @@ import kotlinx.serialization.Serializable
 @Serializable
 data class FlagSet(
     val byteArray: ByteArray = byteArrayOf()
-)
+) {
+    companion object {
+        fun mockFlagSetByteArray(bitCount: Int = 64): ByteArray {
+            val byteSize = (bitCount + 7) / 8 // round up
+            val data = ByteArray(byteSize)
+
+            // For example, just set first few flags
+            for (i in 0 until byteSize) {
+                data[i] = 0b00000001 // only first bit set
+            }
+
+            return data
+        }
+    }
+}
--- src/main/kotlin/core/model/data/PlayerData.kt
+++ src/main/kotlin/core/model/data/PlayerData.kt
@@ -32,8 +32,8 @@ data class PlayerData(
     val admin: Boolean,
     val allianceId: String,
     val allianceTag: String,
-    val flags: FlagSet?,
-    val upgrades: FlagSet?,
+    val flags: ByteArray?,         // deserialized to flagset
+    val upgrades: ByteArray?,      // deserialized to flagset
     val nickname: String,
     val playerSurvivor: String,
     val levelPts: UInt = 0u,
@@ -78,7 +78,7 @@ data class PlayerData(
     val lastLogin: Long?,
     val notifications: List<Notification?>?,
 
-) {
+    ) {
     companion object {
         fun dummy(): PlayerData {
             val srvId = "survivor-player"
@@ -88,8 +88,8 @@ data class PlayerData(
                 admin = true,
                 allianceId = "",
                 allianceTag = "",
-                flags = null,
-                upgrades = null,
+                flags = FlagSet.mockFlagSetByteArray(),
+                upgrades = FlagSet.mockFlagSetByteArray(),
                 nickname = "dzplayer",
                 playerSurvivor = srvId,
                 neighbors = null,
@@ -97,7 +97,15 @@ data class PlayerData(
                 neighborHistory = null,
                 research = null,
                 skills = null,
-                resources = GameResources(cash = 100000, wood = 100, 100, 100, 100, 100, 100),
+                resources = GameResources(
+                    cash = 100000,
+                    wood = 100,
+                    metal = 100,
+                    cloth = 100,
+                    water = 100,
+                    food = 100,
+                    ammunition = 100
+                ),
                 survivors = SurvivorCollection(
                     list = listOf(
                         Survivor.dummy(
@@ -130,7 +138,7 @@ data class PlayerData(
                 dzBounty = null,
                 nextDZBountyIssue = 1_000_000_000,
                 highActivity = null,
-                invsize = 9,
+                invsize = 8,
                 zombieAttack = false,
                 zombieAttackLogins = 0,
                 offersEnabled = false,
@@ -139,5 +147,118 @@ data class PlayerData(
                 notifications = listOf(),
             )
         }
+    }
+
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (javaClass != other?.javaClass) return false
+
+        other as PlayerData
+
+        if (admin != other.admin) return false
+        if (restXP != other.restXP) return false
+        if (bountyCap != other.bountyCap) return false
+        if (lastLogout != other.lastLogout) return false
+        if (nextDZBountyIssue != other.nextDZBountyIssue) return false
+        if (invsize != other.invsize) return false
+        if (zombieAttack != other.zombieAttack) return false
+        if (zombieAttackLogins != other.zombieAttackLogins) return false
+        if (offersEnabled != other.offersEnabled) return false
+        if (lastLogin != other.lastLogin) return false
+        if (key != other.key) return false
+        if (user != other.user) return false
+        if (allianceId != other.allianceId) return false
+        if (allianceTag != other.allianceTag) return false
+        if (!flags.contentEquals(other.flags)) return false
+        if (!upgrades.contentEquals(other.upgrades)) return false
+        if (nickname != other.nickname) return false
+        if (playerSurvivor != other.playerSurvivor) return false
+        if (levelPts != other.levelPts) return false
+        if (oneTimePurchases != other.oneTimePurchases) return false
+        if (neighbors != other.neighbors) return false
+        if (friends != other.friends) return false
+        if (neighborHistory != other.neighborHistory) return false
+        if (research != other.research) return false
+        if (skills != other.skills) return false
+        if (resources != other.resources) return false
+        if (survivors != other.survivors) return false
+        if (playerAttributes != other.playerAttributes) return false
+        if (buildings != other.buildings) return false
+        if (rally != other.rally) return false
+        if (tasks != other.tasks) return false
+        if (missions != other.missions) return false
+        if (assignments != other.assignments) return false
+        if (inventory != other.inventory) return false
+        if (effects != other.effects) return false
+        if (globalEffects != other.globalEffects) return false
+        if (cooldowns != other.cooldowns) return false
+        if (batchRecycles != other.batchRecycles) return false
+        if (offenceLoadout != other.offenceLoadout) return false
+        if (defenceLoadout != other.defenceLoadout) return false
+        if (!quests.contentEquals(other.quests)) return false
+        if (!questsCollected.contentEquals(other.questsCollected)) return false
+        if (!achievements.contentEquals(other.achievements)) return false
+        if (dailyQuest != other.dailyQuest) return false
+        if (questsTracked != other.questsTracked) return false
+        if (gQuestsV2 != other.gQuestsV2) return false
+        if (dzBounty != other.dzBounty) return false
+        if (highActivity != other.highActivity) return false
+        if (prevLogin != other.prevLogin) return false
+        if (notifications != other.notifications) return false
+
+        return true
+    }
+    override fun hashCode(): Int {
+        var result = admin.hashCode()
+        result = 31 * result + restXP
+        result = 31 * result + bountyCap
+        result = 31 * result + (lastLogout?.hashCode() ?: 0)
+        result = 31 * result + nextDZBountyIssue.hashCode()
+        result = 31 * result + invsize
+        result = 31 * result + zombieAttack.hashCode()
+        result = 31 * result + zombieAttackLogins
+        result = 31 * result + offersEnabled.hashCode()
+        result = 31 * result + (lastLogin?.hashCode() ?: 0)
+        result = 31 * result + key.hashCode()
+        result = 31 * result + user.hashCode()
+        result = 31 * result + allianceId.hashCode()
+        result = 31 * result + allianceTag.hashCode()
+        result = 31 * result + (flags?.contentHashCode() ?: 0)
+        result = 31 * result + (upgrades?.contentHashCode() ?: 0)
+        result = 31 * result + nickname.hashCode()
+        result = 31 * result + playerSurvivor.hashCode()
+        result = 31 * result + levelPts.hashCode()
+        result = 31 * result + oneTimePurchases.hashCode()
+        result = 31 * result + (neighbors?.hashCode() ?: 0)
+        result = 31 * result + (friends?.hashCode() ?: 0)
+        result = 31 * result + (neighborHistory?.hashCode() ?: 0)
+        result = 31 * result + (research?.hashCode() ?: 0)
+        result = 31 * result + (skills?.hashCode() ?: 0)
+        result = 31 * result + resources.hashCode()
+        result = 31 * result + survivors.hashCode()
+        result = 31 * result + playerAttributes.hashCode()
+        result = 31 * result + buildings.hashCode()
+        result = 31 * result + (rally?.hashCode() ?: 0)
+        result = 31 * result + tasks.hashCode()
+        result = 31 * result + (missions?.hashCode() ?: 0)
+        result = 31 * result + (assignments?.hashCode() ?: 0)
+        result = 31 * result + (inventory?.hashCode() ?: 0)
+        result = 31 * result + (effects?.hashCode() ?: 0)
+        result = 31 * result + (globalEffects?.hashCode() ?: 0)
+        result = 31 * result + (cooldowns?.hashCode() ?: 0)
+        result = 31 * result + (batchRecycles?.hashCode() ?: 0)
+        result = 31 * result + (offenceLoadout?.hashCode() ?: 0)
+        result = 31 * result + (defenceLoadout?.hashCode() ?: 0)
+        result = 31 * result + (quests?.contentHashCode() ?: 0)
+        result = 31 * result + (questsCollected?.contentHashCode() ?: 0)
+        result = 31 * result + (achievements?.contentHashCode() ?: 0)
+        result = 31 * result + (dailyQuest?.hashCode() ?: 0)
+        result = 31 * result + (questsTracked?.hashCode() ?: 0)
+        result = 31 * result + (gQuestsV2?.hashCode() ?: 0)
+        result = 31 * result + (dzBounty?.hashCode() ?: 0)
+        result = 31 * result + (highActivity?.hashCode() ?: 0)
+        result = 31 * result + (prevLogin?.hashCode() ?: 0)
+        result = 31 * result + (notifications?.hashCode() ?: 0)
+        return result
     }
 }
