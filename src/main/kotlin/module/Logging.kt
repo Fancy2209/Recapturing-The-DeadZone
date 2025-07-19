464391ef00c4191736e1559806bb9f53b3645295
glennhenry <glennhenrybdg05@gmail.com>
2025-07-19 20:43:33 +0700

    Limit long log messages

--- src/main/kotlin/module/Logging.kt
+++ src/main/kotlin/module/Logging.kt
@@ -4,6 +4,7 @@ import io.ktor.server.application.Application
 import io.ktor.server.application.install
 import io.ktor.server.plugins.calllogging.CallLogging
 import io.ktor.server.routing.RoutingContext
+import kotlin.text.decodeToString
 
 fun Application.configureLogging() {
     install(CallLogging)
@@ -11,10 +12,30 @@ fun Application.configureLogging() {
 
 val RoutingContext.log get() = call.application.environment.log
 
+private const val MAX_LOG_LENGTH = 500
+
 fun RoutingContext.logApiMessage(message: Any) {
-    call.application.environment.log.info("Received [API ${call.parameters["path"]}]: $message")
+    val msgString = message.toString()
+    val truncated = if (msgString.length > MAX_LOG_LENGTH) {
+        msgString.take(MAX_LOG_LENGTH) + "... [truncated]"
+    } else {
+        msgString
+    }
+    call.application.environment.log.info("Received [API ${call.parameters["path"]}]: $truncated")
 }
 
 fun RoutingContext.logApiOutput(message: ByteArray) {
-    call.application.environment.log.info("Sent [API ${call.parameters["path"]}]: ${message.decodeToString()}")
+    val msgString = try {
+        message.decodeToString()
+    } catch (e: Exception) {
+        "[binary output not decodable]"
+    }
+
+    val truncated = if (msgString.length > MAX_LOG_LENGTH) {
+        msgString.take(MAX_LOG_LENGTH) + "... [truncated]"
+    } else {
+        msgString
+    }
+
+    call.application.environment.log.info("Sent [API ${call.parameters["path"]}]: $truncated")
 }
