0686928f1a6128789b2cb72b4d82d5fd84eb05ea
glennhenry <glennhenrybdg05@gmail.com>
2025-07-19 22:21:30 +0700

    Mock TimerData for ended time

--- src/main/kotlin/core/model/game/data/TimerData.kt
+++ src/main/kotlin/core/model/game/data/TimerData.kt
@@ -1,10 +1,29 @@
 package dev.deadzone.core.model.game.data
 
+import io.ktor.util.date.getTimeMillis
 import kotlinx.serialization.Serializable
 
 @Serializable
 data class TimerData(
-    val start: Double,
-    val length: Double,
+    val start: Long,
+    val length: Long,
     val data: Map<String, Double>?
-)
+) {
+    companion object {
+        fun dummy(): TimerData {
+            return TimerData(
+                start = 1000,
+                length = 1_000_000,
+                data = mapOf()
+            )
+        }
+
+        fun hasEnded(): TimerData {
+            return TimerData(
+                start = getTimeMillis() - (5 * 60 * 1000), // 5 mins ago
+                length = 1,                                // 1 sec duration
+                data = mapOf()
+            )
+        }
+    }
+}
