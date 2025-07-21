4429ea22d08db85e41ac30cc9034c2a1159b4b24
glennhenry <glennhenrybdg05@gmail.com>
2025-07-21 15:39:46 +0700

    Rename AuthProvider to PlayerRegistry and create an actual AuthProvider

--- src/main/kotlin/core/auth/AuthProvider.kt
+++ src/main/kotlin/core/auth/AuthProvider.kt
@@ -1,7 +1,5 @@
 package dev.deadzone.core.auth
 
-import dev.deadzone.core.data.user.UserData
-
 interface AuthProvider {
-    fun getPlayerById(userId: String): UserData
+    fun authenticate(token: String): String? // userId
 }
--- src/main/kotlin/core/auth/HardcodedAuthProvider.kt
+++ src/main/kotlin/core/auth/HardcodedAuthProvider.kt
@@ -1,18 +1,9 @@
 package dev.deadzone.core.auth
 
 import dev.deadzone.core.data.HardcodedData
-import dev.deadzone.core.data.user.UserData
 
 object HardcodedAuthProvider : AuthProvider {
-    private val users = listOf(
-        UserData(
-            userId = HardcodedData.USER_ID,
-            nickname = HardcodedData.NICKNAME,
-            isAdmin = true
-        ),
-    )
-
-    override fun getPlayerById(userId: String): UserData {
-        return users.find { it.userId == userId } ?: users[0]
+    override fun authenticate(token: String): String? {
+        return if (token == HardcodedData.TOKEN) HardcodedData.USER_ID else null
     }
 }
