ebfa77524fd95c96be12d4adb49b3b805534c3aa
glennhenry <glennhenrybdg05@gmail.com>
2025-07-24 16:04:09 +0700

    Implement the save handler for building move

--- src/main/kotlin/socket/handler/Save.kt
+++ src/main/kotlin/socket/handler/Save.kt
@@ -1,16 +1,24 @@
 package dev.deadzone.socket.handler
 
+import dev.deadzone.core.model.game.data.GameResources
+import dev.deadzone.core.model.game.data.skills.SkillCollection
+import dev.deadzone.core.model.game.data.skills.SkillState
 import dev.deadzone.core.utils.PIOSerializer
-import dev.deadzone.core.utils.parseJsonToMap
+import dev.deadzone.module.Dependency
 import dev.deadzone.module.Logger
 import dev.deadzone.socket.Connection
 import dev.deadzone.socket.ServerContext
 import dev.deadzone.socket.utils.SocketMessage
 import dev.deadzone.socket.utils.SocketMessageHandler
+import io.ktor.util.date.getTimeMillis
+import kotlinx.serialization.EncodeDefault
+import kotlinx.serialization.Serializable
+import kotlinx.serialization.json.Json
 import java.io.File
 import java.io.FileInputStream
 import java.io.InputStreamReader
 import java.util.zip.GZIPInputStream
+import kotlin.collections.mapOf
 
 /**
  * Handle `save` message by:
@@ -35,9 +43,12 @@ class SaveHandler(private val context: ServerContext) : SocketMessageHandler {
         val body = message.getMap("s")
         val data = body?.get("data") as? Map<String, Any?>
         val type = data?.get("_type") as? String
-        val id = body?.get("id") as? String
+        val saveId = body?.get("id") as? String
 
-        Logger.socketPrint("Save message got: type:$type | id:$id")
+        Logger.socketPrint("Save message got: type:$type | id:$saveId")
+
+        // Note: the game typically send and expects JSON data for save message
+        // encode JSON response to string before using PIO serialization
 
         when (type) {
             "get_offers" -> {
@@ -50,9 +61,33 @@ class SaveHandler(private val context: ServerContext) : SocketMessageHandler {
 
             "mis_start" -> {
                 val msg = listOf(
-                    "ss",
-                    missionStartObjectResponse.copy(id = id ?: "")
+                    "r",
+                    saveId ?: "m",
+                    getTimeMillis(),
+                    missionStartObjectResponse.copy(id = saveId ?: "")
                 )
+                send(PIOSerializer.serialize(msg))
+            }
+
+            "bld_move" -> {
+                val x = (data["tx"] as? Number)?.toInt() ?: 0
+                val y = (data["ty"] as? Number)?.toInt() ?: 0
+                val r = (data["rotation"] as? Number)?.toInt() ?: 0
+                val buildingId = data["id"] // use this to save in DB
+                Logger.socketPrint("Building move for $saveId and $buildingId to $x,$y|r:$r")
+
+                val response = SaveBuildingResponse(
+                    success = true, x = x, y = y, r = r
+                )
+                val responseJson = Dependency.json.encodeToString(response)
+
+                val msg = listOf(
+                    "r",
+                    saveId ?: "m",
+                    getTimeMillis(),
+                    responseJson
+                )
+                println(msg)
                 send(PIOSerializer.serialize(msg))
             }
 
@@ -62,6 +97,16 @@ class SaveHandler(private val context: ServerContext) : SocketMessageHandler {
         }
     }
 }
+
+@Serializable
+data class SaveBuildingResponse(
+    val success: Boolean = true,
+    val x: Int,
+    val y: Int,
+    val r: Int,
+    val coins: Int? = null, // used in NetworkMessage.SEND_RESPONSE, interpreted as CASH
+    val skills: Map<String, SkillState>? = null, // used in NetworkMessage.SEND_RESPONSE
+)
 
 fun loadSceneXML(filePath: String): String {
     val file = File(filePath)
@@ -75,6 +120,7 @@ fun loadSceneXML(filePath: String): String {
 val sceneXMLString = loadSceneXML("static/game/data/xml/scenes/street-small-1.xml.gz")
 
 // SaveDataMethod.MISSION_START, MissionData.as line 685
+@Serializable
 data class MissionStartResponse(
     val disabled: Boolean = false,
     val id: String,
@@ -95,6 +141,7 @@ data class MissionStartResponse(
     val allianceAttackerWinPoints: Int
 )
 
+@Serializable
 data class Zombie(
     val id: String,
     val type: String,
