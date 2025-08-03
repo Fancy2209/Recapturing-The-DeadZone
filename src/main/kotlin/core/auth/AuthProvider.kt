f470e8ce9af553f3a65ebb9874cb749388ae511f
glennhenry <glennhenrybdg05@gmail.com>
2025-08-03 21:17:55 +0700

    feat: update AuthProvider and implement a dummy provider

--- src/main/kotlin/core/auth/AuthProvider.kt
+++ src/main/kotlin/core/auth/AuthProvider.kt
@@ -1,5 +1,24 @@
 package dev.deadzone.core.auth
 
+import dev.deadzone.core.auth.model.PlayerSession
+
 interface AuthProvider {
-    fun authenticate(token: String): String? // userId
+    /**
+     * Register a new account with [username] and [password].
+     *
+     * @return [PlayerSession] which includes newly created playerId and a token.
+     */
+    suspend fun register(username: String, password: String): PlayerSession
+
+    /**
+     * Login with [username] and [password].
+     *
+     * @return [PlayerSession] which is used for further authentication.
+     */
+    suspend fun login(username: String, password: String): PlayerSession
+
+    /**
+     * Check whether a user with [username] exists.
+     */
+    suspend fun doesUserExist(username: String): Boolean
 }
---
+++ src/main/kotlin/core/auth/DummyAuthProvider.kt
@@ -0,0 +1,37 @@
+package dev.deadzone.core.auth
+
+import dev.deadzone.core.auth.model.PlayerSession
+import dev.deadzone.core.data.DummyData
+import io.ktor.util.date.getTimeMillis
+
+/**
+ * Dummy auth provider, typically used for dev/testing purpose.
+ * Any operation always succeed.
+ */
+object DummyAuthProvider : AuthProvider {
+    override suspend fun register(
+        username: String,
+        password: String
+    ): PlayerSession {
+        return PlayerSession(
+            playerId = DummyData.PLAYER_ID,
+            token = DummyData.TOKEN,
+            issuedAt = getTimeMillis(),
+            expiresAt = getTimeMillis() + 60 * 1000 * 5 // 5 minutes
+        )
+    }
+
+    override suspend fun login(
+        username: String,
+        password: String
+    ): PlayerSession {
+        return PlayerSession(
+            playerId = DummyData.PLAYER_ID,
+            token = DummyData.TOKEN,
+            issuedAt = getTimeMillis(),
+            expiresAt = getTimeMillis() + 60 * 1000 * 5 // 5 minutes
+        )
+    }
+
+    override suspend fun doesUserExist(username: String): Boolean = true
+}
