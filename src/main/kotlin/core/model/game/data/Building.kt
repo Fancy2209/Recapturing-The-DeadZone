07a58652e8e21e53e9442ee6f31668444219977a
glennhenry <glennhenrybdg05@gmail.com>
2025-08-03 23:24:52 +0700

    feat: enable polymorphic serialization on building object (i.e., for junk building)

--- src/main/kotlin/core/model/game/data/Building.kt
+++ src/main/kotlin/core/model/game/data/Building.kt
@@ -1,14 +1,20 @@
 package dev.deadzone.core.model.game.data
 
+import kotlinx.serialization.ExperimentalSerializationApi
 import kotlinx.serialization.Serializable
-import dev.deadzone.core.model.game.data.TimerData
+import kotlinx.serialization.json.JsonClassDiscriminator
+
+@OptIn(ExperimentalSerializationApi::class)
+@Serializable
+@JsonClassDiscriminator("")
+sealed class BuildingLike
 
 @Serializable
 data class Building(
     val id: String,    // building's unique ID
     val name: String? = null,
     val type: String,  // building's ID in buildings.xml, not to be confused with type in XML
-    val level: Int,
+    val level: Int = 0,
     val rotation: Int,
     val tx: Int,
     val ty: Int,
@@ -16,4 +22,4 @@ data class Building(
     val resourceValue: Double = 0.0,
     val upgrade: TimerData? = null,
     val repair: TimerData? = null
-)
+) : BuildingLike()
