14799108eb428eaa45ee638f6f101a8c289f754e
glennhenry <glennhenrybdg05@gmail.com>
2025-08-06 18:12:40 +0700

    Fix mongodb couldn't use Int as key, by changing key to string

--- src/main/kotlin/core/model/game/data/Survivor.kt
+++ src/main/kotlin/core/model/game/data/Survivor.kt
@@ -24,7 +24,7 @@ data class Survivor(
     val appearance: SurvivorAppearance? = null, // HumanAppearance > SurvivorAppearance
     val scale: Double = 1.22,
     val voice: String,
-    val accessories: Map<Int, String>,  // string is accessory id
+    val accessories: Map<String, String>,  // key is parsed to int, string is accessory id
     val maxClothingAccessories: Int
 ) {
     companion object {
