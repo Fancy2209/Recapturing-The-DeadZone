5b1e35c1649cb1e3fe5046f2daaa1418d766cc32
glennhenry <glennhenrybdg05@gmail.com>
2025-08-07 21:15:26 +0700

    Use custom serializer for polymorphic type: BuildingLike

--- src/main/kotlin/core/model/game/data/Building.kt
+++ src/main/kotlin/core/model/game/data/Building.kt
@@ -1,12 +1,16 @@
 package dev.deadzone.core.model.game.data
 
+import dev.deadzone.module.LogConfigSocketToClient
+import dev.deadzone.module.Logger
+import kotlinx.serialization.DeserializationStrategy
 import kotlinx.serialization.ExperimentalSerializationApi
 import kotlinx.serialization.Serializable
-import kotlinx.serialization.json.JsonClassDiscriminator
-import java.util.UUID
+import kotlinx.serialization.SerializationException
+import kotlinx.serialization.json.*
+import java.util.*
 
 @OptIn(ExperimentalSerializationApi::class)
-@Serializable
+@Serializable(with = BuildingLikeSerializer::class)
 @JsonClassDiscriminator("_t")
 sealed class BuildingLike
 
@@ -24,3 +28,32 @@ data class Building(
     val upgrade: TimerData? = null,
     val repair: TimerData? = null
 ) : BuildingLike()
+
+object BuildingLikeSerializer : JsonContentPolymorphicSerializer<BuildingLike>(BuildingLike::class) {
+    override fun selectDeserializer(element: JsonElement): DeserializationStrategy<BuildingLike> {
+        val discriminator = element.jsonObject["_t"]?.jsonPrimitive?.contentOrNull
+
+        return when (discriminator) {
+            "dev.deadzone.core.model.game.data.Building" -> Building.serializer()
+            "dev.deadzone.core.model.game.data.JunkBuilding" -> JunkBuilding.serializer()
+            null -> {
+                val obj = element.jsonObject
+                return when {
+                    obj.containsKey("items") && obj.containsKey("pos") && obj.containsKey("rot") ->
+                        JunkBuilding.serializer()
+
+                    else ->
+                        Building.serializer()
+                }
+            }
+
+            else -> {
+                Logger.error(
+                    LogConfigSocketToClient,
+                    forceLogFull = true
+                ) { "Error during serialization of BuildingLike type: $element" }
+                throw SerializationException("Unknown type: '$discriminator'")
+            }
+        }
+    }
+}
--- src/main/kotlin/module/Serialization.kt
+++ src/main/kotlin/module/Serialization.kt
@@ -1,17 +1,30 @@
 package dev.deadzone.module
 
+import dev.deadzone.core.model.game.data.Building
+import dev.deadzone.core.model.game.data.BuildingLike
+import dev.deadzone.core.model.game.data.JunkBuilding
 import io.ktor.serialization.kotlinx.json.*
 import io.ktor.serialization.kotlinx.protobuf.*
 import io.ktor.server.application.*
 import io.ktor.server.plugins.contentnegotiation.*
 import kotlinx.serialization.ExperimentalSerializationApi
 import kotlinx.serialization.json.Json
+import kotlinx.serialization.modules.SerializersModule
+import kotlinx.serialization.modules.polymorphic
 import kotlinx.serialization.protobuf.ProtoBuf
 
 @OptIn(ExperimentalSerializationApi::class)
 fun Application.configureSerialization() {
     install(ContentNegotiation) {
+        val module = SerializersModule {
+            polymorphic(BuildingLike::class) {
+                subclass(Building::class, Building.serializer())
+                subclass(JunkBuilding::class, JunkBuilding.serializer())
+            }
+        }
+
         val json = Json {
+            serializersModule = module
             classDiscriminator = "_t"
             prettyPrint = true
             isLenient = true
