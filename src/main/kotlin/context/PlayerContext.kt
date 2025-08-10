8b3b7e7c61591fd936b8d4634d06858b1cd72723
glennhenry <glennhenrybdg05@gmail.com>
2025-08-10 18:25:00 +0700

    Split the context classes

--- src/main/kotlin/context/PlayerContext.kt
+++ src/main/kotlin/context/PlayerContext.kt
@@ -1,32 +1,10 @@
 package dev.deadzone.context
 
-import dev.deadzone.core.auth.AuthProvider
-import dev.deadzone.core.auth.SessionManager
 import dev.deadzone.core.compound.CompoundService
 import dev.deadzone.core.items.InventoryService
 import dev.deadzone.core.survivor.SurvivorService
 import dev.deadzone.data.collection.PlayerAccount
-import dev.deadzone.data.db.BigDB
 import dev.deadzone.socket.Connection
-import dev.deadzone.socket.OnlinePlayerRegistry
-
-/**
- * Holds the global server components. Only single instance of this class is needed in the server.
- */
-data class ServerContext(
-    val db: BigDB,
-    val sessionManager: SessionManager,
-    val onlinePlayerRegistry: OnlinePlayerRegistry,
-    val authProvider: AuthProvider,
-    val config: ServerConfig,
-)
-
-data class ServerConfig(
-    val adminEnabled: Boolean,
-    val useMongo: Boolean,
-    val mongoUrl: String,
-    val isProd: Boolean,
-)
 
 /**
  * A player-scoped data holder. This includes player's socket connection, metadata,
---
+++ src/main/kotlin/context/ServerContext.kt
@@ -0,0 +1,24 @@
+package dev.deadzone.context
+
+import dev.deadzone.core.auth.AuthProvider
+import dev.deadzone.core.auth.SessionManager
+import dev.deadzone.data.db.BigDB
+import dev.deadzone.socket.OnlinePlayerRegistry
+
+/**
+ * Holds the global server components. Only single instance of this class is needed in the server.
+ */
+data class ServerContext(
+    val db: BigDB,
+    val sessionManager: SessionManager,
+    val onlinePlayerRegistry: OnlinePlayerRegistry,
+    val authProvider: AuthProvider,
+    val config: ServerConfig,
+)
+
+data class ServerConfig(
+    val adminEnabled: Boolean,
+    val useMongo: Boolean,
+    val mongoUrl: String,
+    val isProd: Boolean,
+)
