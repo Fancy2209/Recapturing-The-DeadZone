a2b56797b4ca3cedf02a090e58acb7aa04541c4a
glennhenry <glennhenrybdg05@gmail.com>
2025-08-13 01:26:38 +0700

    Fix bug didn't calculate current time correctly in depleteResources

--- src/main/kotlin/core/LazyDataUpdater.kt
+++ src/main/kotlin/core/LazyDataUpdater.kt
@@ -2,6 +2,7 @@ package dev.deadzone.core
 
 import dev.deadzone.core.model.game.data.*
 import io.ktor.util.date.*
+import kotlin.math.ceil
 import kotlin.time.Duration.Companion.milliseconds
 
 /**
@@ -9,13 +10,15 @@ import kotlin.time.Duration.Companion.milliseconds
  */
 object LazyDataUpdater {
     fun depleteResources(lastLogin: Long, res: GameResources): GameResources {
-        val hoursPassed = lastLogin.milliseconds.inWholeHours
-        val depletionRate = 3
-        // depletion formula: each hours deplete res by 3
+        val minutesPassed = (getTimeMillis().milliseconds - lastLogin.milliseconds).inWholeMinutes
+        val depletionRate = 0.05
+        // depletion formula: each minutes deplete res by 0.05, an hour is 3
+
+        val depleted = ceil(minutesPassed * depletionRate)
 
         return res.copy(
-            food = res.food - (hoursPassed * depletionRate).toInt(),
-            water = res.water - (hoursPassed * depletionRate).toInt()
+            food = res.food - (depleted).toInt(),
+            water = res.water - (depleted).toInt()
         )
     }
 
@@ -30,12 +33,23 @@ object LazyDataUpdater {
                 now >= it.start.milliseconds + it.length.milliseconds
             } ?: false
 
-            if (upgradeDone) {
-                bld.copy(level = bld.level + 1, upgrade = null)
-            } else if (repairDone) {
-                bld.copy(repair = null)
-            } else {
-                bld
+            when (bld) {
+                is Building -> when {
+                    upgradeDone -> {
+                        val level = bld.upgrade?.data?.get("level") ?: 1.0
+                        bld.copy(level = level.toInt(), upgrade = null)
+                    }
+                    repairDone -> bld.copy(repair = null)
+                    else -> bld
+                }
+                is JunkBuilding -> when {
+                    upgradeDone -> {
+                        val level = bld.upgrade?.data?.get("level") ?: 1.0
+                        bld.copy(level = level.toInt(), upgrade = null)
+                    }
+                    repairDone -> bld.copy(repair = null)
+                    else -> bld
+                }
             }
         }
     }
