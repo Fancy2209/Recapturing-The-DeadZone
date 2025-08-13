db1b652f6c8cfb2bdb5f0fe669d6b915e37b9b42
glennhenry <glennhenrybdg05@gmail.com>
2025-08-13 18:31:13 +0700

    Change TimerData data value maps to be Any with custom AnyMapSerializer

--- src/main/kotlin/core/model/game/data/TimerData.kt
+++ src/main/kotlin/core/model/game/data/TimerData.kt
@@ -1,8 +1,8 @@
 package dev.deadzone.core.model.game.data
 
+import dev.deadzone.utils.AnyMapSerializer
 import io.ktor.util.date.*
 import kotlinx.serialization.Serializable
-import kotlinx.serialization.json.JsonElement
 import kotlin.time.Duration
 import kotlin.time.Duration.Companion.seconds
 
@@ -10,15 +10,14 @@ import kotlin.time.Duration.Companion.seconds
 data class TimerData(
     val start: Long, // epoch millis
     val length: Long, // length in seconds!
-    /**
-     * use mapOf().toJsonElement()
-     */
-    val data: Map<String, JsonElement>? // this depends on each response. e.g., building upgrade need level
+    // If sending this via API, the value should be JSONElement. Use Json.encodeToJsonElement()
+    @Serializable(with = AnyMapSerializer::class)
+    val data: Map<String, Any>? // this depends on each response. e.g., building upgrade need level
 ) {
     companion object {
         fun runForDuration(
             duration: Duration,
-            data: Map<String, JsonElement>? = emptyMap()
+            data: Map<String, Any>? = emptyMap()
         ): TimerData {
             return TimerData(
                 start = getTimeMillis(),
@@ -28,6 +27,14 @@ data class TimerData(
         }
     }
 }
+
+data class SocketTimerData(
+    val start: Long, // epoch millis
+    val length: Long, // length in seconds!
+    // The value should be JSONElement. Use Json.encodeToJsonElement()
+    @Serializable(with = AnyMapSerializer::class)
+    val data: Map<String, Any>? // this depends on each response. e.g., building upgrade need level
+)
 
 fun TimerData.hasEnded(): Boolean {
     return getTimeMillis() >= this.start + this.length.seconds.inWholeMilliseconds
