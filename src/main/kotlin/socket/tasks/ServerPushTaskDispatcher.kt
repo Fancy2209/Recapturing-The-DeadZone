137df1ad4b7614acc4314fe4cb7936dfc8b13ec0
glennhenry <glennhenrybdg05@gmail.com>
2025-08-13 17:04:15 +0700

    ServerPushTaskDispatcher implements TaskScheduler

--- src/main/kotlin/socket/tasks/ServerPushTaskDispatcher.kt
+++ src/main/kotlin/socket/tasks/ServerPushTaskDispatcher.kt
@@ -4,11 +4,9 @@ import dev.deadzone.socket.core.Connection
 import dev.deadzone.utils.LogConfigSocketError
 import dev.deadzone.utils.LogSource
 import dev.deadzone.utils.Logger
-import kotlinx.coroutines.CompletableDeferred
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Job
-import kotlinx.coroutines.launch
+import kotlinx.coroutines.*
 import kotlin.coroutines.cancellation.CancellationException
+import kotlin.coroutines.coroutineContext
 
 /**
  * Manages and dispatches registered [ServerPushTask]s for a given socket connection.
@@ -17,21 +15,29 @@ import kotlin.coroutines.cancellation.CancellationException
  * to push messages to connected clients (e.g., time update, real-time events).
  *
  * @property tasks keep tracks registered tasks
+ * @property configs config for each task. This will always override the default config
+ * with the given on each [runTask].
  * @property runningTasks List of registered tasks key which are currently run.
  * Each has reference to coroutine job for cancellation.
  * @property taskSignals List of pending tasks which are waiting to be started.
  */
-class ServerPushTaskDispatcher {
+class ServerPushTaskDispatcher : TaskScheduler {
     private val tasks = mutableListOf<ServerPushTask>()
+    private val configs = mutableMapOf<String, TaskConfig>()
     private val runningTasks = mutableMapOf<String, Job>()
     private val taskSignals = mutableMapOf<String, CompletableDeferred<Unit>>()
     private val completionListeners = mutableMapOf<String, MutableList<() -> Unit>>()
 
     fun register(task: ServerPushTask) {
         tasks.add(task)
+        // register the default config
+        this.configs[task.key] = task.config
     }
 
-    fun runTask(taskKey: String) {
+    fun runTask(taskKey: String, configBuilder: (TaskConfig) -> TaskConfig?) {
+        val defaultConfig = requireNotNull(configs[taskKey]) { "Missing task config for taskKey=$taskKey" }
+        val config = configBuilder(defaultConfig) ?: defaultConfig
+        configs[taskKey] = config
         taskSignals.remove(taskKey)?.complete(Unit)
     }
 
@@ -51,7 +57,8 @@ class ServerPushTaskDispatcher {
                 Logger.info(LogSource.SOCKET) { "Push task ${task.key} is ready to run." }
 
                 try {
-                    task.run(connection)
+                    val scheduler = task.scheduler ?: this@ServerPushTaskDispatcher
+                    scheduler.schedule(task, connection)
                     Logger.info(LogSource.SOCKET) { "Push task ${task.key} ran successfully." }
                 } catch (_: CancellationException) {
                     Logger.info(LogSource.SOCKET) { "Push task '${task.key}' was cancelled." }
@@ -64,6 +71,25 @@ class ServerPushTaskDispatcher {
             }
 
             runningTasks[task.key] = job
+        }
+    }
+
+    override suspend fun schedule(
+        task: ServerPushTask,
+        connection: Connection
+    ) {
+        val finalConfig = configs[task.key]!!
+        delay(finalConfig.initialRunDelay)
+
+        val shouldRunInfinitely = finalConfig.repeatDelay != null
+
+        if (shouldRunInfinitely) {
+            while (coroutineContext.isActive) {
+                delay(finalConfig.repeatDelay)
+                task.run(connection)
+            }
+        } else {
+            task.run(connection)
         }
     }
 
