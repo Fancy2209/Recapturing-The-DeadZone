2de5a62b62e6e0546353a0172c80dad96e754d02
glennhenry <glennhenrybdg05@gmail.com>
2025-08-13 23:42:49 +0700

    Add notes about GameDefintions.kt

--- src/main/kotlin/core/data/GameDefinitions.kt
+++ src/main/kotlin/core/data/GameDefinitions.kt
@@ -11,6 +11,24 @@ import kotlin.time.Duration.Companion.milliseconds
 
 /**
  * Global registry that holds game data and config from parsing the game XML resources.
+ *
+ * You will need to read the XML resources in data/xml/. This is to sync with client-side data.
+ * Sometimes the client-side does not send every data (like what zombie it request or what loot it expects)
+ * This is because it expects the server to handle it, including data lookup to those XMLs.
+ *
+ * - For example, items.xml read includes initializing in-memory index such as itemsById, itemsByType, allowing easy retrieval.
+ * - zombie.xml may includes zombies of same type with different level (use this to choose which zombie to spawn depend on area level).
+ * - zombie.xml also have rarity, use this to determine the rarity of spawning that zombie (Yes we need zombie system just like we have the loot system).
+ * - For buildings.xml maybe you will need the data for some buildings like production (reading the prod element) to see its production rate, etc.
+ * - badwords.xml contains useful badword utility. We may use it as verification (not very important here).
+ * - I haven't checked other.
+ *
+ * VERY IMPORTANT: there is confusion around id, type for all XML resources.
+ * - in the server, id is treated as unique identifier,
+ * type as the 'id' in the XML,
+ * and server does not store extra "type" which correspond to the 'type' in XML.
+ * - in the game, lookup to XML is done in syntax like @id == type. This means that the id (at) XML must be equal to type sent from server.
+ * - For type in XML lookup, you can make similar server index like itemsByType
  */
 class GameDefinitions(onResourceLoadComplete: () -> Unit) {
     val itemsById = mutableMapOf<String, ItemResource>()
